c94e82b683b1feb94e2e9b90c6459090abfd9c7d
Aditya <2adijbp@gmail.com>
2022-10-31 14:58:51 -0700

    Completed skeleton theming

---
+++ skeleton/skeleton.js
@@ -0,0 +1,9 @@
+//---------------------------------------------------
+// This function loads the parts of your skeleton
+// (navbar, footer, and other things) into html doc.
+//---------------------------------------------------
+
+(() => {
+  $("#navPlaceholder").load("/skeleton/nav.html");
+  $("#footPlaceholder").load("/skeleton/foot.html");
+})();

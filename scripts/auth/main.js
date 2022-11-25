a88e0da231a0148084b64f23015a0ab94622b709
Mangat Toor <immangat@gmail.com>
2022-11-24 21:15:02 -0800

    Added search functionality

--- scripts/auth/main.js
+++ scripts/auth/main.js
@@ -1,5 +1,16 @@
 var firbaseId;
 
+ var x = document.getElementById("resetButton");
+ function hidebuttons() {
+   console.log("it worked");
+   if (x.style.display == "none") {
+     x.style.display = "block";
+   } else {
+     console.log("button is none");
+     x.style.display = "block";
+   }
+ }
+
 function insertName() {
   firebase.auth().onAuthStateChanged((user) => {
     // Check if a user is signed in:
@@ -8,7 +19,8 @@ function insertName() {
       //gets the template created in the main.html
       let assigntemplate = document.getElementById("assign-template");
 
-      console.log(firbaseId);
+   
+    
 
       //all the assignments with the speific userid
       db.collection("assignments")
@@ -22,9 +34,7 @@ function insertName() {
             var coursename = doc.data().coursename;
             var coursetype = doc.data().assignmenttype;
             var assignmentstatus = doc.data().status;
-            console.log(assignmentID);
-            console.log(date);
-            console.log(firbaseId);
+          
 
             let newcard = assigntemplate.content.cloneNode(true); //what does this mean
 
@@ -50,8 +60,7 @@ function insertName() {
         });
 
       // Do something for the currently logged-in user here:
-      console.log(firbaseId); //print the uid in the browser console
-      console.log(user.displayName); //print the user name in the browser console
+    //print the user name in the browser console
       user_Name = user.displayName;
 
       //method #1:  insert with html only
@@ -66,7 +75,7 @@ function insertName() {
 
 insertName(); //run the function
 
-console.log(firbaseId);
+
 
 // function to get assignments of each user
 // function displayAssignments(collection) {
@@ -101,7 +110,112 @@ console.log(firbaseId);
 
 // displayAssignments("users");
 
+function searchAssignments() {
+  document.getElementById("assignments" + "-go-here").innerHTML = "";
+  
+  hidebuttons();
+
+
+  let stringUser = document.getElementById("searchBar").value;
+
+
+  firebase.auth().onAuthStateChanged((user) => {
+    // Check if a user is signed in:
+    if (user) {
+      let firbaseId2 = user.uid;
+      //gets the template created in the main.html
+      let assigntemplate = document.getElementById("assign-template");
+
+   
+
+      //all the assignments with the speific userid
+      db.collection("assignments")
+        .where("made_by_user", "==", firbaseId2)
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            var assignmentID = doc.id;
+            var assignname = doc.data().name; //gets the value of name key
+            var date = doc.data().date; //gets the value of the date lkey
+            var coursename = doc.data().coursename;
+            var coursetype = doc.data().assignmenttype;
+            var assignmentstatus = doc.data().status;
+          
+
+            let newcard = assigntemplate.content.cloneNode(true); //what does this mean
+            var inassignname = assignname
+              .toLowerCase()
+              .includes(stringUser.toLowerCase());
+
+            var incoursename = coursename
+              .toLowerCase()
+              .includes(stringUser.toLowerCase());
+
+            var incoursetype = coursetype
+              .toLowerCase()
+              .includes(stringUser.toLowerCase());
+
+            var inassignmentstatus = assignmentstatus
+              .toLowerCase()
+              .includes(stringUser.toLowerCase());
+
+              
+
+            if (
+              inassignname ||
+              incoursename ||
+              incoursetype ||
+              inassignmentstatus
+            ) {
+              //update the assigments rows.
+              newcard.querySelector("#assign-name").innerHTML = assignname;
+
+              newcard.querySelector("#assign-date").innerHTML = date;
+
+              newcard.querySelector("#course-name").innerHTML = coursename;
+
+              newcard.querySelector("#assign-type").innerHTML = coursetype;
+
+              newcard.querySelector("#assignment-status").innerHTML =
+                assignmentstatus;
+
+              newcard.querySelector("a").href =
+                "/app/assignments/editAssigment.html?AssignID=" + assignmentID;
+
+              document
+                .getElementById("assignments" + "-go-here")
+                .appendChild(newcard);
+            }
+          });
+        });
+
+      // Do something for the currently logged-in user here:
+//print the user name in the browser console
+      user_Name = user.displayName;
+
+      //method #1:  insert with html only
+      //document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
+      //method #2:  insert using jquery
+      $("#name-goes-here").text(user_Name); //using jquery
+    } else {
+      // No user is signed in.
+    }
+  });
+}
 function setAssignmentData(id) {
   localStorage.setItem("hikeID", id);
-  console.log(id);
+
 }
+
+$("#searchBar").keypress(function (event) {
+  if (event.keyCode === 13) {
+    searchAssignments();
+  }
+});
+
+
+function test() {
+  console.log("Hello it worked");
+}
+
+
